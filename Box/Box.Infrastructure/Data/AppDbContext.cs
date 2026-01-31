505b41264af4e93922fe1e203587801fee610f57
larmdev <78774065+larmdev@users.noreply.github.com>
2026-01-31 16:11:05 +0700

    init user db

--- Box/Box.Infrastructure/Data/AppDbContext.cs
+++ Box/Box.Infrastructure/Data/AppDbContext.cs
@@ -11,6 +11,7 @@ public class AppDbContext : DbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<Course> Courses { get; set; }
     public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
